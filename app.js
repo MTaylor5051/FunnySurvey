26fb84f944703411d55f8454af066db73a3cea95
Michael Taylor <michael.taylor5051@gmail.com>
2016-07-06 17:12:45 -0600

    kkkk

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var http = require('http');
 var path = require('path');
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var urlencoded = require('body-parser').urlencoded;
-var config = require('./config');
 var voice = require('./routes/voice');
 var message = require('./routes/message');
 var results = require('./routes/results');
@@ -25,11 +23,5 @@ app.post('/message', message);
 
 // Ajax route to aggregate response data for the UI
 app.get('/results', results);
-
-// Create HTTP server and mount Express app
-// var server = http.createServer(app);
-// server.listen(config.port, function() {
-//     console.log('Express server started on *:'+config.port);
-// });
 
 module.exports = app;
