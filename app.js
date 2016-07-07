6ab835b915dfe3287000d3e9e881a2cb2b7f6325
Michael Taylor <michael.taylor5051@gmail.com>
2016-07-06 19:56:13 -0600

    dadfa

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var urlencoded = require('body-parser').urlencoded;
-var voice = require('./routes/voice');
-var message = require('./routes/message');
-var results = require('./routes/results');
 
 // initialize MongoDB connection
 require('./models/db');
@@ -17,11 +14,6 @@ app.use(urlencoded({ extended: true }));
 app.use(morgan('combined'));
 
 // Twilio Webhook routes
-app.post('/voice', voice.interview);
-app.post('/voice/:responseId/transcribe/:questionIndex', voice.transcription);
-app.post('/message', message);
-
-// Ajax route to aggregate response data for the UI
-app.get('/results', results);
+require('./routes/router');
 
 module.exports = app;
---
+++ routes/router.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+var router = express.Router();
+var voice = require('./voice');
+var message = require('./message');
+var results = require('./results');
+
+//Recieving data from Twilio
+router.post('/voice', voice.interview);
+router.post('/voice/:responseId/transcribe/:questionIndex', voice.transcription);
+router.post('/message', message);
+
+// Ajax route to aggregate response data for the UI
+app.get('/results', results);
