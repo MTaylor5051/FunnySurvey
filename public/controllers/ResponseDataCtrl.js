8b25fd3bcb8a1dd542a5922388392917274bb797
Michael Taylor <michael.taylor5051@gmail.com>
2016-07-11 21:29:23 -0600

    dfasdf

--- public/controllers/ResponseDataCtrl.js
+++ public/controllers/ResponseDataCtrl.js
@@ -1,3 +1,114 @@
 ﻿app.controller('ResponseDataCtrl', function ($scope, $http) {
+        $scope.optionsChart1 = {
+            chart: {
+                type: 'pieChart',
+                height: 140,
+                donut: false,
+                color: ['#ffa500', '#939393'],
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                showLegend: false,
+                pie: {
+                    startAngle: function(d) { return d.startAngle -Math.PI },
+                    endAngle: function(d) { return d.endAngle -Math.PI }
+                }
+            }
+        };
 
+        $scope.dataChart1 = [
+            {
+                key: "Yes",
+                y: 5
+            },
+            {
+                key: "No",
+                y: 2
+            }
+        ];
+        
+        $scope.optionsChart2 = {
+            chart: {
+                type: 'pieChart',
+                height: 140,
+                donut: false,
+                color: ['#ffa500', '#939393'],
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                showLegend: false,
+                pie: {
+                    startAngle: function(d) { return d.startAngle -Math.PI },
+                    endAngle: function(d) { return d.endAngle -Math.PI }
+                }
+            }
+        };
+
+        $scope.dataChart2 = [
+            {
+                key: "Yes",
+                y: 5
+            },
+            {
+                key: "No",
+                y: 2
+            }
+        ];
+
+        $scope.optionsChart3 = {
+            chart: {
+                type: 'pieChart',
+                height: 140,
+                donut: false,
+                color: ['#ffa500', '#939393'],
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                showLegend: false,
+                pie: {
+                    startAngle: function(d) { return d.startAngle -Math.PI },
+                    endAngle: function(d) { return d.endAngle -Math.PI }
+                }
+            }
+        };
+
+        $scope.dataChart3 = [
+            {
+                key: "Yes",
+                y: 5
+            },
+            {
+                key: "No",
+                y: 2
+            }
+        ];
+
+        $scope.optionsChart4 = {
+            chart: {
+                type: 'pieChart',
+                height: 140,
+                donut: false,
+                color: ['#ffa500', '#939393'],
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                showLegend: false,
+                pie: {
+                    startAngle: function(d) { return d.startAngle -Math.PI },
+                    endAngle: function(d) { return d.endAngle -Math.PI }
+                }
+            }
+        };
+
+        $scope.dataChart4 = [
+            {
+                key: "Yes",
+                y: 5
+            },
+            {
+                key: "No",
+                y: 2
+            }
+        ];
+        
 });
